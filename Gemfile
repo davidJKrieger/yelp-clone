cf1b7023a2c9ff9c29f745386434c1b0e65d03bb
David Krieger <davidkrieger@Davids-MacBook-Air.local>
2019-07-01 18:41:07 -0600

    added user profile link

--- Gemfile
+++ Gemfile
@@ -75,3 +75,7 @@ gem "font-awesome-rails"
 gem 'devise'
 gem "geocoder"
 gem "figaro"
+gem 'carrierwave', '>= 2.0.0.rc', '< 3.0'
+gem 'will_paginate', '~> 3.1'
+gem 'will_paginate-bootstrap', '~> 1.0', '>= 1.0.1'
+gem 'will_paginate-bootstrap4', '~> 0.2.2'
--- db/schema.rb
+++ db/schema.rb
@@ -26,6 +26,17 @@ ActiveRecord::Schema.define(version: 2019_06_25_012411) do
     t.index ["user_id", "place_id"], name: "index_comments_on_user_id_and_place_id"
   end
 
+  create_table "photos", id: :serial, force: :cascade do |t|
+    t.text "caption"
+    t.integer "place_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "picture"
+    t.index ["place_id"], name: "index_photos_on_place_id"
+    t.index ["user_id", "place_id"], name: "index_photos_on_user_id_and_place_id"
+  end
+
   create_table "places", force: :cascade do |t|
     t.string "name"
     t.text "description"
