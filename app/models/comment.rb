0e54d0bdc0706e9b949b1c8299b0b366295a5b4a
David Krieger <davidkrieger@Davids-MacBook-Air.local>
2019-09-18 15:09:12 -0600

    config email

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ApplicationRecord
     belongs_to :user
     belongs_to :place
+    after_create :send_comment_email
 
     RATINGS = {
         'Not Recommended': '1_star',
@@ -8,7 +9,11 @@ class Comment < ApplicationRecord
         'Highly Recommended': '3_stars'
   }
 
-    def humanized_rating
+  def humanized_rating
     RATINGS.invert[self.rating]
+  end
+
+  def send_comment_email
+    NotificationMailer.comment_added(self).deliver_now 
   end
 end
